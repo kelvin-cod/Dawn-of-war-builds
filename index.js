f9ce1bd3cc57f5ad4f1e677ff22f175c7319597d
Kelvin augusto <kelvin.zuzor@gmail.com>
2023-04-05 19:03:43 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-process.title = 'MyWebServer';
-var args = process.argv,
-    port = args[2] || 7070,
-    webServer = require('./server');
-
-webServer.listen(port, function () {
-    console.log('Server started at port ' + port);
-});
